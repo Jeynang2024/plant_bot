8302558186b05d3f0f5799771e8d52a48b4c661a
Jey Nang Gogoi <156996004+Jeynang2024@users.noreply.github.com>
2025-03-13 12:07:27 +0530

    Create api_bot_connect.py

---
+++ api_bot_connect.py
@@ -0,0 +1,69 @@
+import os
+from fastapi import FastAPI
+
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+app = FastAPI()
+
+
+HF_TOKEN=os.environ.get("HF_TOKEN")
+HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
+DB_FAISS_PATH="vectorstore/db_faiss"
+embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+def load_llm(huggingface_repo_id):
+    llm=HuggingFaceEndpoint(
+        repo_id=huggingface_repo_id,
+        temperature=0.5,
+        model_kwargs={"token":HF_TOKEN,
+                      "max_length":"512"}
+    )
+    return llm
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+def set_custom_prompt(custom_prompt_template):
+    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+
+qa_chain=RetrievalQA.from_chain_type(
+    llm=load_llm(HUGGINGFACE_REPO_ID),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k':3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt':set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+@app.get("/query/")
+async def query_faiss(question: str):
+    response = qa_chain.invoke({'query': question}) #here calling the function from new_user_voice .this file recording the audio convertig to text nd returning the text as question
+    #if only texting then only directly run this file
+    print("response:",response)
+    return {
+        "answer": response["result"],
+        "sources": response["source_documents"]
+    }
+@app.get("/")
+async def root():
+    return {"message": "FastAPI is running! Use /query/ to send questions."}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
