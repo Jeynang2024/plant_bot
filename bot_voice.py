c3b97c8ed7dae6b76b3ef9f2159e62a48f8156d0
Jey Nang Gogoi <156996004+Jeynang2024@users.noreply.github.com>
2025-03-13 21:10:10 +0530

    Create bot_voice.py

---
+++ bot_voice.py
@@ -0,0 +1,12 @@
+import os
+from gtts import gTTS
+
+def text_to_speech_with_gtts_old(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
